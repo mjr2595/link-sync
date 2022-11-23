9b95ed98240d8a92cc7946ae46d75873c7b98cf6
mjr2595 <michaeljohnraymond@gmail.com>
2022-11-23 16:49:49 -0600

    fixed share buttons and some other formatting

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
-const mainShare = document.querySelector('.share-button')
 const shareButtons = document.querySelectorAll('.tile-share-button')
-console.log(shareButtons)
 
 async function copyText(e) {
     //prevent button going to site
     e.preventDefault()
     const link = this.getAttribute('link')
-    console.log(link)
     try {
         await navigator.clipboard.writeText(link)
-        alert("copied! " + link)
+        alert("Link copied: " + link)
     } catch (err) {
         console.error(err)
     }
