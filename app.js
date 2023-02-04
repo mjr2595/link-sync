f10131f0233d9e01e1c142254686804a8e89e89a
mjr2595 <michaeljohnraymond@gmail.com>
2023-02-04 15:03:50 -0600

    chirp chirp

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-const shareButtons = document.querySelectorAll('.tile-share-button')
+const shareButtons = document.querySelectorAll(".tile-share-button");
 
 async function copyText(e) {
-    //prevent button going to site
-    e.preventDefault()
-    const link = this.getAttribute('link')
-    try {
-        await navigator.clipboard.writeText(link)
-        alert("Link copied: " + link)
-    } catch (err) {
-        console.error(err)
-    }
+  //prevent button going to site
+  e.preventDefault();
+  const link = this.getAttribute("link");
+  try {
+    await navigator.clipboard.writeText(link);
+    alert("Link copied: " + link);
+  } catch (err) {
+    console.error(err);
+  }
 }
 
-shareButtons.forEach(shareButton => shareButton.addEventListener('click', copyText))
+shareButtons.forEach((shareButton) => shareButton.addEventListener("click", copyText));
