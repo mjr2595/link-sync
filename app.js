426e37fd4d3c17f27c230e6fce4f3f064fbf516e
mjr2595 <michaeljohnraymond@gmail.com>
2023-02-26 12:42:31 -0600

    resume and tippy tooltips added

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
-const shareButtons = document.querySelectorAll(".tile-share-button");
+// Select all share buttons with link (doesn't include resume button)
+const shareButtons = document.querySelectorAll(".tile-share-button[link]");
 
+// Tooltip for share buttons
+tippy(shareButtons, {
+  content: "Copy link 🔗",
+});
+
+// Tooltip for resume button
+tippy("#resume", {
+  content: "Resume 🤓",
+  placement: "left",
+});
+
+// copy link to clipboard
 async function copyText(e) {
   //prevent button going to site
   e.preventDefault();
   const link = this.getAttribute("link");
   try {
     await navigator.clipboard.writeText(link);
-    alert("Link copied: " + link);
   } catch (err) {
     console.error(err);
   }
 }
-
 shareButtons.forEach((shareButton) => shareButton.addEventListener("click", copyText));
+
+tippy(shareButtons, {
+  trigger: "click",
+  content: "Copied! 🔥",
+});
